31ce724bc6a4c2d244ad30718fe6a9dc5949dc70
David Serrano <davidsegu9@gmail.com>
2019-06-05 15:16:17 -0600

    Core api y addManager Cliente

---
+++ Quiz-lab2-lab3/CoreAPI/ClienteManager.cs
@@ -0,0 +1,42 @@
+﻿using DataAcess.Crud;
+using Entities_POJO;
+using Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreAPI
+{
+    class ClienteManager : BaseManager
+    {
+        private ClienteCrudFactory crudCliente;
+
+        public ClienteManager()
+        {
+            crudCliente = new ClienteCrudFactory();
+        }
+
+        public void Create(Cliente cliente)
+        {
+            try
+            {
+                var c = crudCliente.Retrieve<Cliente>(cliente);
+                if (c != null)
+                {
+                    throw new BussinessException(3);
+                }
+
+                crudCliente.Create(cliente);
+            }
+            catch (Exception ex)
+            {
+                ExceptionManager.GetInstance().Process(ex);
+
+            }
+
+        }
+
+    }
+}

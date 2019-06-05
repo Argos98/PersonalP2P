f5a75755322d5f419b07e581e52e0ea86e180c80
David Serrano <davidsegu9@gmail.com>
2019-06-05 10:58:19 -0600

    actualizar pojos para lab5

---
+++ Quiz-lab2-lab3/BaseEntity/Tarjeta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseEntity
+{
+    public class Tarjeta
+    {
+        String NumeroTarjeta { get; set; }
+        String Fecha { get; set; }
+        int CVV { get; set; }
+
+
+
+        public Tarjeta()
+        {
+
+        }
+    }
+}
